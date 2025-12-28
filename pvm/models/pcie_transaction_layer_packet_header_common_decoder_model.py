a4ad9412f8d73222fb6601a454435018ebd70669
Nickolas Upole <nick.at.night@icloud.com>
2025-12-27 19:04:39 -0500

    Add pcie_transaction_layer_packet_header_common_decoder_model

---
+++ pvm/models/pcie_transaction_layer_packet_header_common_decoder_model.py
@@ -0,0 +1,17 @@
+import cocotb
+
+import pyuvm
+
+from pvm.decoders.pcie_transaction_layer_packet_header_common_decoder import PcieTransactionLayerPacketHeaderCommonDecoder
+
+class PcieTransactionLayerPacketHeaderCommonDecoderModel(pyuvm.uvm_subscriber):
+    def build_phase(self):
+        self._decoder = PcieTransactionLayerPacketHeaderCommonDecoder(int(cocotb.top.UPSTREAM_WORD_WIDTH.value))
+        self.analysis_port = pyuvm.uvm_analysis_port('analysis_port', self)
+
+    def write(self, indexed_word):
+        word_index, word = indexed_word
+        word_index, word = (int(word_index), int(word))
+        word_valid, word, word_error = self._decoder.decode(word_index, word)
+        if word_valid:
+            self.analysis_port.write(word + (word_error,))
